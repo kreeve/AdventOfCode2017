4893b06543faee7687e485e1af7528e9317e6319
Reeve <reevekyl@amazon.com>
2017-12-11 17:42:36 -0500

    Solved (ish) day 10

---
+++ day10.hs
@@ -0,0 +1,121 @@
+import System.IO
+import Data.Char
+import qualified Data.Set as Set
+
+import Test.QuickCheck
+
+import Util
+
+data ListState = ListState Int Int Int [Int] [Int] deriving (Show, Eq) -- Index, len, skip, lens, list
+
+-- Slice a list from start to end
+slice :: Int -> Int -> [Int] -> [Int]
+slice start end lst =
+  let
+    trueEnd = if end > start then end else (end+(length lst))
+    double = take ((length lst)*2) $ cycle lst
+    front = take trueEnd double
+  in
+    drop start front
+
+-- Rebuild a list, with the sublist starting at `start`
+rebuild :: Int -> [Int] -> [Int] -> [Int]
+rebuild start sub lst =
+  let
+    m = length sub
+    n = length lst
+    end = (mod (m + start) n)
+    numInFront = if start < end
+      then 0
+      else (m + start) - n
+    back = if numInFront == 0
+      then drop end lst
+      else take (m - numInFront) sub -- All if doesn't wrap
+    mid = if end < start && end > 0
+      then slice end start lst
+      else take (m - numInFront) sub
+    front = if numInFront > 0
+      then drop (m - numInFront) sub
+      else take start lst
+  in
+    take n $ front ++ mid ++ back
+    
+
+-- Reverse part of the list
+revPart :: [Int] -> Int -> Int -> [Int]
+revPart lst start len =
+  let
+    end = mod (start+len) ((length lst ))
+    sub = reverse $ slice start end lst
+  in
+    rebuild start sub lst
+
+-- Transform the state (one step)
+update :: ListState -> ListState
+update (ListState idx len skip lens lst) =
+  let
+    n = length lst
+    lst' = revPart lst idx len
+    idx' = mod (idx + len + skip) n
+    len' = case lens of
+      x:xs -> x
+      [] -> len
+    lens' = case lens of
+      x:xs -> xs
+      [] -> lens
+    skip' = skip + 1
+  in
+    ListState idx' len' skip' lens' lst'
+
+part1 :: ListState -> Int
+part1 (ListState _ _ _ _ lst) = case lst of
+  x:y:xs -> x*y
+  _ -> 0
+
+lstOf :: ListState -> [Int]
+lstOf (ListState idx len skip lens lst) = lst
+
+stateFrom :: [Int] -> Int -> ListState
+stateFrom lens num = ListState 0 (head lens) 0 (tail lens) (take num [0,1..])
+
+finalState :: ListState -> ListState
+finalState state = case state of
+  (ListState idx len skip lens lst) ->
+    let
+      n = length lens
+      func = foldr (.) id (replicate (n+1) update)
+    in
+      func state
+
+preservedProp lst =
+  (length lst) > 0 && (length (filter (\x->x<0) lst)) == 0 ==>
+  Set.fromList (lstOf (finalState (stateFrom lst 10))) == Set.fromList (lstOf (stateFrom lst 10))  
+
+(//) :: Show a => Ord a => [a] -> [(Int, a)] -> [a]
+(//) lst idxs = map (\i -> case lookup i idxs of
+                        Just a -> a
+                        Nothing -> lst !! i) (take (length lst) [0,1..])
+
+step :: ListState -> Int -> ListState
+step (ListState p0 _ s0 _ v0) n = ListState p1 0 s1 [] v1
+  where
+    ixes = (\x -> mod (x+p0) (length v0)) <$> init [0 .. n]
+    vals = (\x -> v0 !! x) <$> ixes
+    v1   = v0 // zip ixes (reverse vals)
+    p1   = mod (p0 + n + s0) (length v0)
+    s1   = s0 + 1
+
+process :: [Int] -> Int -> ListState
+process lens num = foldl step start lens
+  where
+    start = ListState 0 0 0 [] (take num [0,1..])
+    
+main :: IO()
+main = do
+  line <- getLine
+  let
+    vals = map (readInt) ((split ',') line)
+    n = length vals
+    state = ListState 0 (head vals) 0 (tail vals) (take 5 [0,1..])
+  print $ vals
+  print $ product . (take 2) $ lstOf (process vals 256)
---
+++ util.hs
@@ -0,0 +1,99 @@
+module Util(replace
+           , argMax
+           , fixedPoint
+           , fst3
+           , findCycle
+           , remChar
+           , allSame
+           , majority
+           , split
+           , readInt) where
+import Data.Maybe
+import qualified Data.Set as Set
+-- Take an index to a sequence, replace that index w/ modified value
+replace :: Int -> Int -> [Int] -> [Int]
+replace idx val seq = (take idx seq) ++ [val] ++ (drop (idx+1) seq)
+
+
+-- Find index with largest value.
+argMax :: Ord a => [a] -> (Int,a)
+argMax lst =
+  let
+    n = length lst
+    m = foldr1 max lst
+    zipped = zip (take (length lst) [0,1..]) lst
+  in
+    let
+      bestTuple = head (Prelude.filter (\x -> (snd x)==m) zipped)
+    in
+      bestTuple
+
+-- Find a fixed point of a function, starting at `val`
+fixedPoint :: Eq a => (a -> a) -> a -> a
+fixedPoint func start
+  | start == val = val
+  | otherwise = fixedPoint func val
+  where
+    val = func start
+
+-- Grab first element of a 3tuple
+fst3 :: (a,b,c) -> a
+fst3 (x,y,z) = x
+
+-- Helper function
+findCycleHelper :: Eq a => (a->a) -> (a,a) -> Int -> Maybe (a,Int)
+findCycleHelper func (tort,hare) num
+  | t2 == h2 = Just (t2,num)
+  | t2 == tort || h2 == hare = Nothing -- Two separate fixed points
+  | otherwise = findCycleHelper func (t2,h2) (num + 1)
+  where
+    t2 = func tort
+    h2 = func (func hare)
+
+-- Set version of helper
+findCycleSetHelper :: (Eq a,Ord a) => (a->a) -> a -> Set.Set a -> Int -> Int
+findCycleSetHelper func val seen num
+  | Set.member val seen = num
+  | otherwise = findCycleSetHelper func v2 s2 num+1
+  where
+    v2 = func val
+    s2 = Set.union seen (Set.fromList [val])
+
+-- Find a cycle in a function. Sort of like in a list, but `next` is function application.
+-- Using `tortoise and hare` algorithm
+findCycle :: (Eq a, Ord a) => (a->a) -> a -> Int
+findCycle func start =
+  let
+    res = findCycleSetHelper func start (Set.fromList []) 0
+  in
+    res
+
+-- Remove a char from a str (any list actually)
+remChar :: Eq a => a -> [a] -> [a]
+remChar ch str = Prelude.filter (\x -> x /= ch) str
+
+-- All elements same in list?
+allSame :: Eq a => [a] -> Bool
+allSame [] = True
+allSame [_] = True
+allSame (x:y:xs) = (x == y) && allSame (y:xs)
+
+-- Majority of a list
+majority :: Eq a => [a] -> a
+majority lst =
+  let
+    count e = length (filter (\x -> x == e) lst)
+    counts = map (\e -> count e) lst
+    (idx,_) = argMax counts
+  in
+    lst !! idx
+
+-- Parse commas
+split :: Char -> String -> [String]
+split _ "" = []
+split c s = firstWord : (split c rest)
+    where firstWord = takeWhile (/=c) s
+          rest = drop (length firstWord + 1) s
+
+readInt :: String -> Int
+readInt = read
