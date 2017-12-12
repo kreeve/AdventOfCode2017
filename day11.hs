1c16ea1099ed1ef713895c845fef90aea704842e
Kyle Reeve <krzw92@gmail.com>
2017-12-12 13:11:43 -0500

    Update to my (beautiful) day 11 solution :)

--- day11.hs
+++ day11.hs
@@ -1,3 +1,8 @@
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Util
+import Heap
+
 type Pos = (Int, Int)
 type Dir = (Int, Int)
 
@@ -35,36 +40,72 @@ allDirs = [north, south, east, west, northeast, southeast, northwest, southwest]
 neighbors :: Pos -> [Pos]
 neighbors pos = map (\d -> move pos d) allDirs
 
-smallestVertex :: [(Pos,Int)] -> (Pos,Int)
+smallestVertex :: [(Int,Pos)] -> (Int,Pos)
 smallestVertex q =
   let
-    minVal = foldr1 min (map snd q)
+    q' = reverse q
+    minVal = foldr1 min (map snd q')
   in
-    head (filter (\x -> (snd x) == minVal) q)
-
-contained :: [(Pos,Int)] -> Pos -> Bool
-contained q p = case lookup p q of
-  Nothing -> False
-  Just _ -> True
+    head (filter (\x -> (snd x) == minVal) q')
 
 inList :: Eq a => [a] -> a -> Bool
 inList [] _ = True
 inList (x:xs) y = (x /= y) && (inList xs y)
 
+manhattanDist :: Pos -> Pos -> Int
+manhattanDist (x,y) (z,w) =
+  (abs (x-z)) + (abs (y-w))
+
 -- Shortest path from point a to point b
-shortestPath :: (Pos,Int) -> Pos -> [(Pos,Int)] -> [Pos] -> Int
-shortestPath (cur,cost) dest queue seen
+shortestPath :: (Int,Pos) -> Pos -> Set.Set (Int,Pos) -> (Set.Set Pos) -> Map.Map Pos Int -> Int
+shortestPath (cost,cur) dest queue seen memo
   | cur == dest = cost
-  | otherwise = 
-    let
-      seen' = seen ++ [cur]
-      nbs = neighbors cur -- Our neighbors
-      neighborDist = cost + 1
-      toAdd = filter ((inList seen)) nbs
-      nq = queue ++ map (\x -> (x,neighborDist)) toAdd
-      next = smallestVertex nq
-      nq' = filter (\x -> x /= next) nq
-    in
-      shortestPath next dest nq' seen'
+  | otherwise =
+    case Map.lookup cur memo of
+      Just d -> d
+      Nothing ->
+        let
+          seen' = seen `Set.union` (Set.fromList [cur])
+          nbs = neighbors cur -- Our neighbors
+          neighborDist = cost + 1
+          toAdd = filter (\x -> not (Set.member x seen)) nbs
+          nq = Set.union (Set.fromList (map (\x -> (neighborDist,x)) toAdd)) queue
+          next = Set.findMin nq
+          d = manhattanDist cur dest
+          nq' = Set.filter (\x -> x /= next && ((manhattanDist (snd x) dest) < d)) nq
+        in
+          shortestPath next dest nq' seen' memo
       
-    
+readDir :: String -> Dir
+readDir s = case s of
+  "n" -> north
+  "s" -> south
+  "e" -> east
+  "w" -> west
+  "ne" -> northeast
+  "nw" -> northwest
+  "se" -> southeast
+  "sw" -> southwest
+
+-- Memoized calculation
+manyDist :: [Pos] -> Map.Map Pos Int -> [Int]
+manyDist [] _ = []
+manyDist (x:xs) memo =
+  let
+    dist = shortestPath (0,(0,0)) x Set.empty Set.empty memo
+    memo' = Map.insert x dist memo
+  in
+    dist:(manyDist xs memo')
+  
+main :: IO()
+main =
+  do
+    line <- getLine
+    let
+      dirs = map readDir (split ',' line)
+      finalPos = foldr move (0,0) dirs
+      allPos = scanl move (0,0) dirs
+      allDist = map (\p -> shortestPath (0,p) (0,0) Set.empty Set.empty Map.empty) allPos 
+    print $ finalPos
+    print $ shortestPath (0,(0,0)) finalPos Set.empty Set.empty Map.empty
+    print $ foldr1 max allDist
